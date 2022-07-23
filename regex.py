c4e18abbe994d4c2a780b9a4f189a536c8aeaa76
jummy22222 <42906679+jummy22222@users.noreply.github.com>
2022-07-23 19:34:58 +0200

    Add files via upload

--- regex.py
+++ regex.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+import time
 
-
+lo=random.randint(0,19)
 # define a main function
 def main():
     # initialize the pygame module
@@ -9,7 +11,7 @@ def main():
     pygame.display.set_caption("minimal program")
 
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240, 180))
+    screen = pygame.display.set_mode((400, 400))
 
     # define a variable to control the main loop
     running = True
@@ -18,9 +20,45 @@ def main():
     while running:
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
-            tlo = pygame.image.load("objekt1.jpg")
-            screen.blit(tlo, (0, 0))
+            class kwadrat:
+                def __init__(self, xowe, ykowe):
+                    self.xowe=xowe
+                    self.ykowe=ykowe
+                    self.dotyk=0
+                    self.image=pygame.image.load("pelny.png")
+            with open("nazwy.txt", "r") as nazwy:
+                names = nazwy.readlines()
+            pion=0
+            poziom=0
+            for y in range(len(names)):
+                names[y] = names[y].replace(" ", " ")
+                names[y] = names[y].replace("\n", "_")
+            names = dict(zip(names, [None]*len(names)))
+            print(names)
+            for i in names:
+                names[i] = pygame.image.load("pusty.png")
+                pion+=1
+                if pion <= 19:
+                    screen.blit(names[i], (pion*20, poziom*20))
+                else:
+                    pion=0
+                    poziom+=1
+                    screen.blit(names[i], (pion * 20, poziom*20))
+            jd = pygame.image.load("pusty.png")
+            screen.blit(jd, (0, 0))
             pygame.display.flip()
+            with open("nazwy.txt", "r") as nazwy:
+                imiona = nazwy.readlines()
+                for y in range(len(imiona)):
+                    imiona[y] = imiona[y].replace(" ", " ")
+                    imiona[y] = imiona[y].replace("\n", "_")
+            imiona = dict(zip(names, [None] * len(names)))
+            for x in imiona:
+                imiona[x] = kwadrat(lo, 0)
+                screen.blit(imiona[x].image, (imiona[x].xowe, imiona[x].ykowe))
+                pygame.display.flip()
+                time.sleep(10)
+
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
