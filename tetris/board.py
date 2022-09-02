537f3e98f3f577d0190ba31f8911b7d2bb555fa1
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-09-02 22:35:38 +0200

    mniej wiecej dziala

--- tetris/board.py
+++ tetris/board.py
@@ -5,9 +5,9 @@ class Board:
     def __init__(self):
         self.board = np.vstack((np.zeros((18, 15)), [2 for i in range(15)]))
 
-    def update_board(self) -> int:
+    def update_board(self):
         for c,i in enumerate(self.board):
             if np.array_equal(i, np.ones(15)):
-                print(c)
-                np.delete(self.board, c, 0)
+                self.board = np.delete(self.board, c, 0)
+                self.board = np.insert(self.board, 0, np.zeros(15), 0)
 
