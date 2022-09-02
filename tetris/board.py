3df97714a9e721fcf74a2741318ee2eb16df29c9
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-09-02 18:53:39 +0200

    .

--- tetris/board.py
+++ tetris/board.py
@@ -3,6 +3,7 @@ import numpy as np
 class Board:
 
     def __init__(self):
-        self.board = np.zeros((23, 15))
-
-
+        upper_rows = np.zeros((18, 15))
+        bottom_row = [2 for i in range(15)]
+        self.board = np.vstack((upper_rows, bottom_row))
+        print(self.board)
--- tetris/main.py
+++ tetris/main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from game import Game
 
 RED = (255, 0, 0)
@@ -19,12 +20,18 @@ while run:
     instruction_text1 = instruction_font1.render('A/D rotate brick             L-arrow/R-arrow - move brick', True, RED)
     window.blit(instruction_text1, (10, 580))
 
+    for x in range(15):
+        for y in range(19):
+            if game.game_board.board[y, x] == 0:
+                pygame.draw.rect(window, (152,245,255), (x*30, y*30, 30, 30))
+            if game.game_board.board[y, x] == 2:
+                pygame.draw.rect(window, (102,205,0), (x * 30, y * 30, 30, 30))
+
     if game.current_brick is None:
         game.new_brick()
 
     for i in game.current_brick.brick_fields:
         pygame.draw.rect(window, game.current_brick.color, (i[0], i[1], 30, 30))
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,6 +49,7 @@ while run:
                 game.current_brick.move_down(30)
 
     game.current_brick.move_down(1)
+
 
     pygame.display.update()
     window.fill((0, 0, 0))
