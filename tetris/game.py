001310ea5942cce6836bada24b53a5c33a8e3828
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-09-03 10:48:13 +0200

    komentarze

--- tetris/game.py
+++ tetris/game.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 from board import Board
 from brick import Brick
@@ -14,9 +15,12 @@ class Game:
     def new_brick(self):
         self.current_brick = Brick(30 * random.randint(1,13), 0)
 
-    def game_over(self) -> bool:
-        for i in self.game_board.board[0]:
-            if i == 1:
-                return True
-        return False
+    def brick_fell(self):
+        for i in self.current_brick.brick_fields:
+            m = self.game_board.board[math.floor(i[1] / 30) + 1, math.floor(i[0] / 30)]
+            if m == 1 or m == 2:  # sprawdzenie czy klocek spadl
+                for j in self.current_brick.brick_fields:
+                    self.game_board.board[math.floor(j[1] / 30), math.floor(j[0] / 30)] = 1
+                self.current_brick = None
+                break
 
--- tetris/main.py
+++ tetris/main.py
@@ -1,11 +1,10 @@
 import pygame
-import math
 from game import Game
 
 RED = (255, 0, 0)
 
 pygame.init()
-window = pygame.display.set_mode((450, 600))
+window = pygame.display.set_mode((450, 600))        #inicjalizacja okna gry
 pygame.display.set_caption("TETRIS")
 run = True
 time = pygame.time.Clock()
@@ -14,27 +13,27 @@ game = Game()
 
 while run:
 
-    pygame.time.delay(int(game.speed)) #predkosc gry
+    pygame.time.delay(int(game.speed))      #predkosc gry
 
     pygame.font.init()
     instruction_font = pygame.font.SysFont('instruction font', 25)
-    instruction_text = instruction_font.render('A/D -rotate brick            L-arrow/R-arrow - move brick', True, RED) #wyswietlanie instrukcji
+    instruction_text = instruction_font.render('A/D -rotate brick            L-arrow/R-arrow - move brick', True, RED)        #wyswietlanie instrukci
     window.blit(instruction_text, (10, 580))
 
     for x in range(15):
         for y in range(19):
             if game.game_board.board[y, x] == 0:
                 pygame.draw.rect(window, (152, 245, 255), (x*30, y*30, 30, 30))
-            if game.game_board.board[y, x] == 2:
+            if game.game_board.board[y, x] == 2:                                        #rysowanie tla i klockow ktore spadly
                 pygame.draw.rect(window, (102, 205, 0), (x * 30, y * 30, 30, 30))
             if game.game_board.board[y, x] == 1:
                 pygame.draw.rect(window, (255, 185, 15), (x * 30, y * 30, 30, 30))
 
-    if game.current_brick is None:
+    if game.current_brick is None:         #tworzenie nowego kolcka jesli poprzedni jest juz na dole
         game.new_brick()
 
     for i in game.current_brick.brick_fields:
-        pygame.draw.rect(window, game.current_brick.color, (i[0], i[1], 30, 30))
+        pygame.draw.rect(window, game.current_brick.color, (i[0], i[1], 30, 30))      #rysowanie aktualnie spadajacego kolcka
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,7 +41,7 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 game.current_brick.rotate_left()
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d:                                 #sterowanie ruchem klocka
                 game.current_brick.rotate_right()
             if event.key == pygame.K_LEFT:
                 game.current_brick.move_left()
@@ -51,27 +50,26 @@ while run:
             if event.key == pygame.K_DOWN:
                 game.current_brick.move_down(30)
 
-    game.current_brick.move_down(1)
-    for i in game.current_brick.brick_fields:
-        if game.game_board.board[math.floor(i[1] / 30) + 1, math.floor(i[0]/30)] == 1 or game.game_board.board[math.floor(i[1] / 30) + 1, math.floor(i[0]/30)] == 2:
-            for j in game.current_brick.brick_fields:
-                game.game_board.board[math.floor(j[1] / 30), math.floor(j[0]/30)] = 1
-            game.current_brick = None
-            break
+    game.current_brick.move_down(1)                 #spadanie klocka
+
+    game.brick_fell()         #spradzenie czy klocek spadl
 
     if game.game_board.update_board():
-        game.score += 15
+        game.score += 15                                #usuwanie wypelnionych klockami rzedow i zwiekszanie wyniku
         game.speed = 40 - game.score * 0.2
 
     score_font = pygame.font.SysFont('score font', 35)
     score_text = score_font.render("Score = " + str(game.score), True, RED)  # wyswietlanie wyniku
     window.blit(score_text, (10, 10))
 
-    if game.game_over():
+    if max(game.game_board.board[0]):
         game_over_font = pygame.font.SysFont('game over font', 50)
         game_over_text = game_over_font.render("GAME OVER", True, RED)
-        window.blit(game_over_text, (125, 200))
-
+        window.blit(game_over_text, (125, 200))                                     #wyswietlanie 'game over'
+        play_again_font = pygame.font.SysFont('play again', 50)
+        play_again_text = play_again_font.render("press enter to play again", True, RED)
+        window.blit(play_again_text, (30, 230))
 
     pygame.display.update()
     window.fill((0, 0, 0))
+
