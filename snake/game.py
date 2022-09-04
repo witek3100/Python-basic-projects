7301fa4ca031d59e0742930815e90c3258bc797b
Witek Nowogórski <nowogorski@student.agh.edu.pl>
2022-09-04 16:39:47 +0200

    .

--- snake/game.py
+++ snake/game.py
@@ -1,26 +1,40 @@
 from snake import Snake
 import pygame
+import random
 
 class Game:
 
     def __init__(self):
         self.sneak = Snake()
         self.score = 0
+        self.point = self.new_point()
 
     def controls(self) -> bool:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP and self.sneak.move_direction != [0, 1]:
                     self.sneak.move_direction = [0, -1]
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and self.sneak.move_direction != [1, 0]:
                     self.sneak.move_direction = [-1, 0]
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT and self.sneak.move_direction != [-1, 0]:
                     self.sneak.move_direction = [1, 0]
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and self.sneak.move_direction != [0, -1]:
                     self.sneak.move_direction = [0, 1]
         return True
 
-    def update_score(self):
-        self.score = len(self.sneak.snake_fields) - 2
+    def new_point(self) -> []:
+        return [random.randint(1, 22), random.randint(1, 22)]
+
+    def point_earned(self):
+        if self.point == [self.sneak.x, self.sneak.y]:
+            self.score += 10
+            self.point = None
+            return True
+        return False
+
+    def game_over(self) -> bool:
+        for i in self.sneak.snake_fields:
+            if i == [self.sneak.x, self.sneak.y]:
+                return True
--- snake/main.py
+++ snake/main.py
@@ -1,8 +1,10 @@
 import pygame
 from game import Game
 
+
 GRAY = (48, 48, 48)
 GREEN = (124, 252, 0)
+RED =(255, 0, 0)
 
 pygame.init()
 window = pygame.display.set_mode((750, 750))
@@ -13,7 +15,7 @@ game = Game()
 run = True
 while run:
 
-    pygame.time.delay(500)
+    pygame.time.delay(200)
 
     pygame.draw.rect(window, GRAY, (0, 0, 30, 750))
     pygame.draw.rect(window, GRAY, (0, 0, 750, 30))
@@ -22,14 +24,30 @@ while run:
 
     run = game.controls()
 
+    if game.point == None:
+        game.point = game.new_point()
+    pygame.draw.circle(window, RED, (game.point[0]*30 + 15, game.point[1]*30 + 15), 15, 0)
+
     pygame.draw.rect(window, GREEN, (game.sneak.x * 30, game.sneak.y * 30, 30, 30))
     for i in game.sneak.snake_fields:
         pygame.draw.rect(window, GREEN, (i[0]*30, i[1]*30, 30, 30))
 
     game.sneak.move()
     game.sneak.extend_snake = False
-    if :
+    if game.point_earned():
         game.sneak.extend_snake = True
+
+    score_font = pygame.font.SysFont('score font', 25)
+    score_text = score_font.render("SCORE = " + str(game.score), True, RED)  # wyswietlanie wyniku
+    window.blit(score_text, (10, 10))
+
+    if game.game_over():
+        game_over_font = pygame.font.SysFont('game over font', 50)
+        game_over_text = game_over_font.render("GAME OVER", True, RED)  # wyswietlanie wyniku
+        window.blit(game_over_text, (300, 300))
+        play_agian_font = pygame.font.SysFont('play again font', 30)
+        play_again_text = play_agian_font.render("your score: " + str(game.score) + "  press enter to play again", True, RED)
+        window.blit(play_again_text, (250, 330))
 
     pygame.display.update()
     window.fill((0,0,0))
